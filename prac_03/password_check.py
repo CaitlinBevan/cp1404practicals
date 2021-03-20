356c892ec839e19fb5468c4229c6ecab506d402b
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-03-20 15:52:00 +1000

    Refactor functions of password check program and add comments

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -2,26 +2,23 @@ MINIMUM_LENGTH = 5
 
 
 def main():
+    """Get password and display using stars."""
     print(f"Your password must be a minimum length of {MINIMUM_LENGTH} characters")
     password = get_password()
     display_stars(password)
 
 
 def get_password():
+    """Get valid password."""
     password = input("Please enter your password: ")
-    while not is_valid(password):
+    while len(password) < MINIMUM_LENGTH:
         print("Invalid password")
         password = input("Please enter your password: ")
     return password
 
 
-def is_valid(password):
-    if len(password) < MINIMUM_LENGTH:
-        return False
-    return True
-
-
 def display_stars(word):
+    """Display word length using stars."""
     for i in range(len(word)):
         print("*", end="")
 
