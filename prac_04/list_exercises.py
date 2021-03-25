c20472bf1ee256dc7e242b3058b05a79202853f7
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-03-25 21:20:37 +1000

    Add woefully inadequate security checker to list exercises program

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,18 +1,31 @@
-"""Basic list operations"""
-total = 0
+#  1. Basic list operations
+# total = 0
+#
+# numbers = [0] * 5
+# print("Please enter 5 numbers")
+# for i in range(5):
+#     numbers[i] = int(input("Number: "))
+#
+# print(f"The first number is {numbers[0]}")
+# print(f"The last number is {numbers[-1]}")
+# print(f"The smallest number is {min(numbers)}")
+# print(f"The largest number is {max(numbers)}")
+#
+# for number in numbers:
+#     total += number
+# average = total / len(numbers)
+#
+# print(f"The average of the numbers is {average}")
 
-numbers = [0] * 5
-print("Please enter 5 numbers")
-for i in range(5):
-    numbers[i] = int(input("Number: "))
 
-print(f"The first number is {numbers[0]}")
-print(f"The last number is {numbers[-1]}")
-print(f"The smallest number is {min(numbers)}")
-print(f"The largest number is {max(numbers)}")
+#  2. Woefully inadequate security checker
 
-for number in numbers:
-    total += number
-average = total / len(numbers)
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface',
+             'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface',
+             'StartServer', 'bob']
 
-print(f"The average of the numbers is {average}")
+username = input("Enter Username: ")
+if username not in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
