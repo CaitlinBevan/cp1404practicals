f23e0e4173f56b559221386d910330fedf06dfc7
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-03-25 21:15:42 +1000

    Add print statements and average to list exercises function

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -1,8 +1,18 @@
 """Basic list operations"""
-
+total = 0
 
 numbers = [0] * 5
 print("Please enter 5 numbers")
 for i in range(5):
     numbers[i] = int(input("Number: "))
-print(numbers)
+
+print(f"The first number is {numbers[0]}")
+print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
+print(f"The largest number is {max(numbers)}")
+
+for number in numbers:
+    total += number
+average = total / len(numbers)
+
+print(f"The average of the numbers is {average}")
