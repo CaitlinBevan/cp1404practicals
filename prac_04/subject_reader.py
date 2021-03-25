938fc61b375b17d1351d1676a7cd77891d639939
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-03-25 20:44:35 +1000

    Modify program to return data as a list of lists and add function to display subject data

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -7,12 +7,14 @@ FILENAME = "subject_data.txt"
 
 
 def main():
+    """Subject reader program."""
     data = get_data()
-    print(data)
+    display_subject(data)
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
+    data = []
     input_file = open(FILENAME)
     for line in input_file:
         print(line)  # See what a line looks like
@@ -23,7 +25,15 @@ def get_data():
         parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
         print(parts)  # See if that worked
         print("----------")
+        data.append(parts)
     input_file.close()
+    return data
+
+
+def display_subject(data):
+    """Display subject data."""
+    for subject_data in data:
+        print(f"{subject_data[0]} is taught by {subject_data[1]:<12s}  and has  {subject_data[2]:3.0f} students")
 
 
 main()
