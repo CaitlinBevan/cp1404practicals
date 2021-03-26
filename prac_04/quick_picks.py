1b3a3f449655b3eaea6a4335cd67913e95b2bc71
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-03-27 09:09:41 +1000

    Add loop to quick picks program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,1 +1,15 @@
 """Quick Pick Lottery Ticket Generator"""
+
+import random
+
+MINIMUM_NUMBER = 1
+MAXIMUM_NUMBER = 45
+TOTAL_NUMBERS = 6
+
+total_quick_picks = int(input("How many quick picks? "))
+quick_picks = [0] * total_quick_picks
+for i in range(total_quick_picks):
+    for j in range(TOTAL_NUMBERS):
+        results = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        print(f"{results:2.0f}", end=" ")
+    print()
