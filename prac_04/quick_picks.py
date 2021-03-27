77ee47e3d53327db1447d2352241acee88db8b5a
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-03-27 11:46:43 +1000

    Append result to quick picks list

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -7,9 +7,12 @@ MAXIMUM_NUMBER = 45
 TOTAL_NUMBERS = 6
 
 total_quick_picks = int(input("How many quick picks? "))
-quick_picks = [0] * total_quick_picks
+quick_picks = []
 for i in range(total_quick_picks):
     for j in range(TOTAL_NUMBERS):
-        results = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
-        print(f"{results:2.0f}", end=" ")
+        result = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
+        quick_picks.append(result)
+        quick_picks.sort()
+        print(f"{result:2.0f}", end=" ")
     print()
+# TODO: sort results
