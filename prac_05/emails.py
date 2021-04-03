251a3562a4dd4c35cbeee860b115f879194c06ac
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-04-03 11:20:07 +1000

    Refactor to print dictionary and include option to change name

--- prac_05/emails.py
+++ prac_05/emails.py
@@ -5,18 +5,24 @@ Emails
 
 
 def main():
-    email_dict = {}
+    emails_dict = {}
     email = input("Email: ")
     while email != "":
         name = get_name(email)
-        print(f"Is your name {name}? (Y/n) ")
+        suggested_name = input(f"Is your name {name}? (Y/n) ").upper()
+        if suggested_name != "Y" and suggested_name != "":
+            name = input("Name: ")
+        emails_dict[email] = name
         email = input("Email: ")
+
+    for email, name in emails_dict.items():
+        print(f"{name} ({email})")
 
 
 def get_name(email):
     split_email = email.split("@")[0]
-    email_name = split_email.split(".")
-    name = " ".join(email_name).title()
+    further_split = split_email.split(".")
+    name = " ".join(further_split).title()
     return name
 
 
