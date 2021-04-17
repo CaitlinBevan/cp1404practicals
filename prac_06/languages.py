843985ffa4b15b1961fcdca8667f4040a97129aa
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-04-17 15:39:07 +1000

    Add Languages program

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+"""CP1404/CP5632 Practical - Programming Language."""
+
+
+from prac_06.programming_language import ProgrammingLanguage
+
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+
+main()
