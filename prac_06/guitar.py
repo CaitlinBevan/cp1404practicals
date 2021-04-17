78e7b62827fc8ddee14813496ce7365ee96d2b84
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-04-17 17:55:12 +1000

    Modify is_vintage method and add doc strings

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -4,17 +4,22 @@
 class Guitar:
     """Represent a Guitar object."""
     YEAR = 2021
+    VINTAGE = 50
 
     def __init__(self, name="", year=0, cost=0):
+        """Initialise a Guitar instance."""
         self.name = name
         self.year = year
         self.cost = cost
 
     def __str__(self):
+        """Return string formatting."""
         return "{self.name} ({self.year}) : ${self.cost}".format(self=self)
 
     def get_age(self):
+        """Return age of Guitar."""
         return Guitar.YEAR - self.year
 
-    def is_vintage(self, get_age):
-        return get_age(self) >= 50
+    def is_vintage(self):
+        """Return if age greater than or equal to Guitar.VINTAGE."""
+        return self.get_age() >= Guitar.VINTAGE
