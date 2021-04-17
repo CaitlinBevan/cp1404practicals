05d9969593c1ff2179a9a498dfc105616d1a3447
Caitlin <caitlin.bevan@my.jcu.edu.au>
2021-04-17 16:57:21 +1000

    Add print statement to languages program

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -1,17 +1,23 @@
-"""CP1404/CP5632 Practical - Programming Language."""
+"""CP1404/CP5632 Practical - Languages Program."""
 
 
 from prac_06.programming_language import ProgrammingLanguage
 
 
 def main():
+    """Languages Program."""
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
 
     languages = [ruby, python, visual_basic]
+    # for language in languages:
+    #     print(language)
+
+    print("The dynamically typed languages are:")
     for language in languages:
-        print(language)
+        if language.is_dynamic():
+            print(language.name)
 
 
 main()
