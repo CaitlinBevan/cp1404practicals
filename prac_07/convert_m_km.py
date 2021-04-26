2433dc2a82f75485ac8c07a254220d86d4fb3b67
CaitlinBevan <caitlin.bevan@my.jcu.edu.au>
2021-04-26 19:42:30 +1000

    Calculate the conversion from miles to kilometres and display in the bottom label

--- prac_07/convert_m_km.py
+++ prac_07/convert_m_km.py
@@ -10,10 +10,15 @@ from kivy.lang import Builder
 class ConvertMtoKm(App):
     """ConvertMtoKm is a Kivy App for to convert miles to kilometres."""
     def build(self):
-        """ build the Kivy app from the kv file """
+        """Build the Kivy app from the kv file."""
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_m_km.kv')
         return self.root
+
+    def handle_calculate(self, value):
+        """Handle calculation (could be button press or other call), output result to label widget."""
+        result = value * 1.60934
+        self.root.ids.output_label.text = str(result)
 
 
 ConvertMtoKm().run()
