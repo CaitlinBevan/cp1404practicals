6cdb304161d5d2a88b9ed35aefe87d738ac21e37
CaitlinBevan <caitlin.bevan@my.jcu.edu.au>
2021-04-28 21:00:35 +1000

    Modify convert_m_km program to handle invalid inputs

--- prac_07/convert_m_km.py
+++ prac_07/convert_m_km.py
@@ -6,6 +6,8 @@ Kivy GUI program to convert miles to kilometers
 from kivy.app import App
 from kivy.lang import Builder
 
+CONVERSION_RATE = 1.60934
+
 
 class MilesToKilometres(App):
     """MilesToKilometres is a Kivy App for to convert miles to kilometres."""
@@ -15,16 +17,24 @@ class MilesToKilometres(App):
         self.root = Builder.load_file('convert_m_km.kv')
         return self.root
 
-    def handle_calculate(self, value):
+    def handle_calculate(self):
         """Handle calculation (could be button press or other call), output result to label widget."""
-        result = value * 1.60934
+        miles = self.get_valid_input()
+        result = miles * CONVERSION_RATE
         self.root.ids.output_label.text = str(result)
 
     def handle_increment(self, increment):
         """Handle button press to increase by one or decrease by one."""
-        result = 3 + increment
+        result = self.get_valid_input() + increment
         self.root.ids.input_number.text = str(result)
-        self.handle_calculate(result)
+        self.get_valid_input()
+
+    def get_valid_input(self):
+        try:
+            result = int(self.root.ids.input_number.text)
+            return result
+        except ValueError:
+            return 0
 
 
 MilesToKilometres().run()
