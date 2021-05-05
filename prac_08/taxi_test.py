008318ceb33a54c16c141e940d4b0354d21e89e4
CaitlinBevan <caitlin.bevan@my.jcu.edu.au>
2021-05-05 21:05:07 +1000

    Add the class variable price_per_km to the Taxi class (in taxi.py) and set it to 1.23

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -6,7 +6,8 @@ from prac_08.taxi import Taxi
 
 def main():
     """Test code for Taxi Class."""
-    new_taxi = Taxi("Prius 1", 100, 1.23)
+    # new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi = Taxi("Prius 1", 100)
     new_taxi.drive(40)
     print(new_taxi)
 
