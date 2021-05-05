7822b311b7503cb7d958cca81bffabc1641dd866
CaitlinBevan <caitlin.bevan@my.jcu.edu.au>
2021-05-05 21:00:06 +1000

    Create Car Class and Taxi Class and create taxi_test program to test Taxi Class

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,18 @@
+"""CP1404/CP5632 Practical
+Used Car"""
+
+from prac_08.taxi import Taxi
+
+
+def main():
+    """Test code for Taxi Class."""
+    new_taxi = Taxi("Prius 1", 100, 1.23)
+    new_taxi.drive(40)
+    print(new_taxi)
+
+    new_taxi.start_fare()
+    new_taxi.drive(100)
+    print(new_taxi)
+
+
+main()
