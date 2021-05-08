c30baafd7bd540ac9df1d114cf2609190c9e59c2
CaitlinBevan <caitlin.bevan@my.jcu.edu.au>
2021-05-08 20:40:19 +1000

    Add code for taxi simulator menu choices

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -5,6 +5,8 @@ Taxi Simulator
 from prac_08.taxi import Taxi
 from prac_08.silver_service_taxi import SilverServiceTaxi
 
+MENU = "q)uit, c)hoose taxi, d)rive"
+
 
 def main():
     """Taxi Program."""
@@ -13,28 +15,43 @@ def main():
     bill_to_date = 0.00
 
     print("Let's drive!")
-    print("q)uit, c)hoose taxi, d)rive")
+    print(MENU)
     choice = input(">>> ").lower()
     while choice != "q":
         if choice == "c":
             print("Taxis available:")
-            for i, taxi in enumerate(taxis):
-                print(i, taxi)
+            display_available_taxis(taxis)
+            taxi_choice = int(input("Choose taxi: "))
+            try:
+                current_taxi = taxis[taxi_choice]
+            except IndexError:
                 print("Invalid taxi choice")
-            print("Bill to date: ")
+            print(f"Bill to date: ${bill_to_date:.2f}")
         elif choice == "d":
-            print("Drive how far? ")
-            print("Your Limo trip cost you $102.90")
-            print("Bill to date: $682.60")
+            if current_taxi is None:
+                print("You need to choose a taxi before you can drive")
+            else:
+                current_taxi.start_fare()
+                distance_to_drive = int(input("Drive how far? "))
+                current_taxi.drive(distance_to_drive)
+                current_fare = current_taxi.get_fare()
+                print(f"Your {current_taxi.name} trip cost you ${current_fare:.2f}")
+                bill_to_date += current_fare
+            print(f"Bill to date: ${bill_to_date:.2f}")
         else:
             print("Invalid option")
-        print("Let's drive!")
-        print("q)uit, c)hoose taxi, d)rive")
+            print(f"Bill to date: ${bill_to_date:.2f}")
+        print(MENU)
         choice = input(">>> ").lower()
-    print("Total trip cost: ")
+    print(f"Total trip cost: {bill_to_date:.2f}")
     print("Taxis are now:")
+    display_available_taxis(taxis)
+
+
+def display_available_taxis(taxis):
+    """Display available taxis."""
     for i, taxi in enumerate(taxis):
-        print(i, taxi)
+        print(f"{i} - {taxi}")
 
 
 main()
