4001464ba2179d6c71c5ad8bd7c3ec5a15f17c2e
CaitlinBevan <caitlin.bevan@my.jcu.edu.au>
2021-05-09 23:00:46 +1000

    Modify taxi_simulator using feedback provided in code review

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -21,26 +21,24 @@ def main():
         if choice == "c":
             print("Taxis available:")
             display_available_taxis(taxis)
-            taxi_choice = int(input("Choose taxi: "))
+            taxi_choice = get_valid_number("Choose taxi: ")
             try:
                 current_taxi = taxis[taxi_choice]
             except IndexError:
                 print("Invalid taxi choice")
-            print(f"Bill to date: ${bill_to_date:.2f}")
         elif choice == "d":
-            if current_taxi is None:
-                print("You need to choose a taxi before you can drive")
-            else:
+            if current_taxi:
                 current_taxi.start_fare()
-                distance_to_drive = int(input("Drive how far? "))
+                distance_to_drive = get_valid_number("Drive how far? ")
                 current_taxi.drive(distance_to_drive)
                 current_fare = current_taxi.get_fare()
                 print(f"Your {current_taxi.name} trip cost you ${current_fare:.2f}")
                 bill_to_date += current_fare
-            print(f"Bill to date: ${bill_to_date:.2f}")
+            else:
+                print("You need to choose a taxi before you can drive")
         else:
             print("Invalid option")
-            print(f"Bill to date: ${bill_to_date:.2f}")
+        print(f"Bill to date: ${bill_to_date:.2f}")
         print(MENU)
         choice = input(">>> ").lower()
     print(f"Total trip cost: {bill_to_date:.2f}")
@@ -52,6 +50,16 @@ def display_available_taxis(taxis):
     """Display available taxis."""
     for i, taxi in enumerate(taxis):
         print(f"{i} - {taxi}")
+
+
+def get_valid_number(prompt):
+    """Get a valid number."""
+    finished = False
+    while not finished:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Invalid input; enter a valid number")
 
 
 main()
